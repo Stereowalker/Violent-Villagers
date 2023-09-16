8df7aceb341bbb662ee98351affefeb64cfb3599
Stereowalker <stereowalker.mc@gmail.com>
2023-09-16 08:38:51 +0100

    Update CarriableGenericMixin.java

--- src/main/java/com/stereowalker/violentvillagers/mixin/carrier/CarriableGenericMixin.java
+++ src/main/java/com/stereowalker/violentvillagers/mixin/carrier/CarriableGenericMixin.java
@@ -10,22 +10,34 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 import com.stereowalker.violentvillagers.ViolentVillagers;
 import com.stereowalker.violentvillagers.tags.BlockVTags;
 
-import me.steven.carrier.api.CarrierComponent;
-import me.steven.carrier.impl.CarriableGeneric;
 import net.minecraft.core.BlockPos;
 import net.minecraft.world.InteractionResult;
 import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.state.BlockState;
 
-@Mixin(CarriableGeneric.class)
+//1.19.2
+//@Mixin(me.steven.carrier.impl.CarriableGeneric.class)
+//1.19.3+
+@Mixin(me.steven.carrier.impl.blocks.BaseCarriableBlock.class)
 public abstract class CarriableGenericMixin {
 
-	@Inject(method = "tryPickup", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/level/Level;removeBlockEntity(Lnet/minecraft/core/BlockPos;)V"))
+	//1.19.2
+	/*@Inject(method = "tryPickup", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/level/Level;removeBlockEntity(Lnet/minecraft/core/BlockPos;)V"))
 	public void tryPickup_inject(@NotNull CarrierComponent carrier, @NotNull Level world, @NotNull BlockPos blockPos, @Nullable Entity entity, CallbackInfoReturnable<InteractionResult> cir) {
 		BlockState blockState = world.getBlockState(blockPos);
 		if (blockState.is(BlockVTags.GUARDED_BY_VILLAGERS) && carrier != null) {
 			ViolentVillagers.upsetNearbyVillagers(carrier.getOwner(), blockPos, false);
+        }
+	}*/
+	
+	//1.19.3+
+	@Inject(method = "tryPickup", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/level/Level;removeBlockEntity(Lnet/minecraft/core/BlockPos;)V"))
+	public void tryPickup_inject(@NotNull Player player, @NotNull Level world, @NotNull BlockPos blockPos, @Nullable Entity entity, CallbackInfoReturnable<InteractionResult> cir) {
+		BlockState blockState = world.getBlockState(blockPos);
+		if (blockState.is(BlockVTags.GUARDED_BY_VILLAGERS) && player != null) {
+			ViolentVillagers.upsetNearbyVillagers(player, blockPos, false);
         }
 	}
 }
