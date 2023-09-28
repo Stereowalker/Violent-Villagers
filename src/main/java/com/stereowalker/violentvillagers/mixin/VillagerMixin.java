635f6beae2f71bf16cd5481718a8f3beb57eef73
Stereowalker <stereowalker.mc@gmail.com>
2023-09-28 17:53:28 +0100

    Changed overwrite method to inject and redirect

--- src/main/java/com/stereowalker/violentvillagers/mixin/VillagerMixin.java
+++ src/main/java/com/stereowalker/violentvillagers/mixin/VillagerMixin.java
@@ -7,11 +7,12 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Overwrite;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.At.Shift;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import com.google.common.collect.ImmutableList;
-import com.google.common.collect.ImmutableSet;
 import com.mojang.datafixers.util.Pair;
 import com.stereowalker.violentvillagers.world.entity.ai.sensing.VSensorType;
 
@@ -29,7 +30,6 @@ import net.minecraft.world.entity.ai.behavior.StartAttacking;
 import net.minecraft.world.entity.ai.behavior.StopAttackingIfTargetInvalid;
 import net.minecraft.world.entity.ai.behavior.VillagerGoalPackages;
 import net.minecraft.world.entity.ai.memory.MemoryModuleType;
-import net.minecraft.world.entity.ai.memory.MemoryStatus;
 import net.minecraft.world.entity.ai.sensing.Sensor;
 import net.minecraft.world.entity.ai.sensing.SensorType;
 import net.minecraft.world.entity.npc.AbstractVillager;
@@ -38,7 +38,6 @@ import net.minecraft.world.entity.npc.VillagerDataHolder;
 import net.minecraft.world.entity.npc.VillagerProfession;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.entity.schedule.Activity;
-import net.minecraft.world.entity.schedule.Schedule;
 import net.minecraft.world.level.Level;
 
 @Mixin(Villager.class)
@@ -80,42 +79,31 @@ public abstract class VillagerMixin extends AbstractVillager implements Reputati
         }*/
         return vill.getBrain().getMemory(MemoryModuleType.NEAREST_ATTACKABLE);
     }
-	
+    
     /**
-     * @param villagerBrain
+     * This is registered after Activity.CORE to ensure that fighting is their utmost priority
      */
-    @Overwrite
-    private void registerBrainGoals(Brain<Villager> villagerBrain) {
-        VillagerProfession villagerProfession = this.getVillagerData().getProfession();
-        if (this.isBaby()) {
-            villagerBrain.setSchedule(Schedule.VILLAGER_BABY);
-            villagerBrain.addActivity(Activity.PLAY, VillagerGoalPackages.getPlayPackage(0.5f));
-        } else {
-            villagerBrain.setSchedule(Schedule.VILLAGER_DEFAULT);
-            villagerBrain.addActivityWithConditions(Activity.WORK, VillagerGoalPackages.getWorkPackage(villagerProfession, 0.5f), ImmutableSet.of(Pair.of(MemoryModuleType.JOB_SITE, MemoryStatus.VALUE_PRESENT)));
-        }
-        villagerBrain.addActivity(Activity.CORE, VillagerGoalPackages.getCorePackage(villagerProfession, 0.5f));
-        //1.19.2
-        //villagerBrain.addActivityAndRemoveMemoryWhenStopped(Activity.FIGHT, 0, ImmutableList.of(new StopAttackingIfTargetInvalid<Villager>(VillagerMixin::onStopAttacking), new SetWalkTargetFromAttackTargetIfTargetOutOfReach(/*AxolotlAi::getSpeedModifierChasing*/1.0f), new MeleeAttack(20), new EraseMemoryIf<Villager>(VillagerMixin::continueAttacking, MemoryModuleType.ATTACK_TARGET)), MemoryModuleType.ATTACK_TARGET);
-        //1.19.3+
-        villagerBrain.addActivityAndRemoveMemoryWhenStopped(Activity.FIGHT, 0, ImmutableList.of(StopAttackingIfTargetInvalid.create(VillagerMixin::onStopAttacking), SetWalkTargetFromAttackTargetIfTargetOutOfReach.create(/*AxolotlAi::getSpeedModifierChasing*/1.0f), MeleeAttack.create(20), EraseMemoryIf.create(VillagerMixin::continueAttacking, MemoryModuleType.ATTACK_TARGET)), MemoryModuleType.ATTACK_TARGET);
-        villagerBrain.addActivityWithConditions(Activity.MEET, VillagerGoalPackages.getMeetPackage(villagerProfession, 0.5f), ImmutableSet.of(Pair.of(MemoryModuleType.MEETING_POINT, MemoryStatus.VALUE_PRESENT)));
-        villagerBrain.addActivity(Activity.REST, VillagerGoalPackages.getRestPackage(villagerProfession, 0.5f));
-        //1.19.2
-        //ImmutableList<? extends Pair<Integer, ? extends Behavior<? super Villager>>> ent = new ImmutableList.Builder<Pair<Integer,? extends Behavior<? super Villager>>>().add(Pair.of(3, new StartAttacking<Villager>(VillagerMixin::findNearestValidAttackTarget))).addAll(VillagerGoalPackages.getIdlePackage(villagerProfession, 0.5f)).build();
-        //1.19.3+
-        ImmutableList<? extends Pair<Integer, ? extends BehaviorControl<? super Villager>>> ent = new ImmutableList.Builder<Pair<Integer, ? extends BehaviorControl<? super Villager>>>().add(Pair.of(3, StartAttacking.create(VillagerMixin::findNearestValidAttackTarget))).addAll(VillagerGoalPackages.getIdlePackage(villagerProfession, 0.5f)).build();
-        villagerBrain.addActivity(Activity.IDLE, ent);
-        villagerBrain.addActivity(Activity.PANIC, VillagerGoalPackages.getPanicPackage(villagerProfession, 0.5f));
-        villagerBrain.addActivity(Activity.PRE_RAID, VillagerGoalPackages.getPreRaidPackage(villagerProfession, 0.5f));
-        villagerBrain.addActivity(Activity.RAID, VillagerGoalPackages.getRaidPackage(villagerProfession, 0.5f));
-        villagerBrain.addActivity(Activity.HIDE, VillagerGoalPackages.getHidePackage(villagerProfession, 0.5f));
-        villagerBrain.setCoreActivities(ImmutableSet.of(Activity.CORE));
-        villagerBrain.setDefaultActivity(Activity.IDLE);
-        villagerBrain.setActiveActivityIfPossible(Activity.IDLE);
-        villagerBrain.updateActivityFromSchedule(this.level().getDayTime(), this.level().getGameTime());
+    @Inject(method = "registerBrainGoals", at = @At(value = "INVOKE", shift = Shift.AFTER, ordinal = 1, target = "Lnet/minecraft/world/entity/ai/Brain;addActivity(Lnet/minecraft/world/entity/schedule/Activity;Lcom/google/common/collect/ImmutableList;)V"))
+    protected void registerBrainGoals1(Brain<Villager> villagerBrain, CallbackInfo ci) {
+    	//1.19.2
+      //villagerBrain.addActivityAndRemoveMemoryWhenStopped(Activity.FIGHT, 0, ImmutableList.of(new StopAttackingIfTargetInvalid<Villager>(VillagerMixin::onStopAttacking), new SetWalkTargetFromAttackTargetIfTargetOutOfReach(/*AxolotlAi::getSpeedModifierChasing*/1.0f), new MeleeAttack(20), new EraseMemoryIf<Villager>(VillagerMixin::continueAttacking, MemoryModuleType.ATTACK_TARGET)), MemoryModuleType.ATTACK_TARGET);
+    	//1.19.3+
+    	villagerBrain.addActivityAndRemoveMemoryWhenStopped(Activity.FIGHT, 0, ImmutableList.of(StopAttackingIfTargetInvalid.create(VillagerMixin::onStopAttacking), SetWalkTargetFromAttackTargetIfTargetOutOfReach.create(/*AxolotlAi::getSpeedModifierChasing*/1.0f), MeleeAttack.create(20), EraseMemoryIf.create(VillagerMixin::continueAttacking, MemoryModuleType.ATTACK_TARGET)), MemoryModuleType.ATTACK_TARGET);
+          
     }
-
+    
+    /**
+     * This is overwrites Activity.IDLE to allow our villager to enter a state of violence later
+     */
+    @Redirect(method = "registerBrainGoals", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/entity/ai/behavior/VillagerGoalPackages;getIdlePackage(Lnet/minecraft/world/entity/npc/VillagerProfession;F)Lcom/google/common/collect/ImmutableList;"))
+    protected ImmutableList<?> registerBrainGoals2(VillagerProfession villagerProfession, float speedModifier) {
+    	//1.19.2
+      //return new ImmutableList.Builder<Pair<Integer,? extends Behavior<? super Villager>>>().add(Pair.of(3, new StartAttacking<Villager>(VillagerMixin::findNearestValidAttackTarget))).addAll(VillagerGoalPackages.getIdlePackage(villagerProfession, speedModifier)).build();
+    	//1.19.3+
+    	return new ImmutableList.Builder<Pair<Integer,? extends BehaviorControl<? super Villager>>>().add(Pair.of(3, StartAttacking.create(VillagerMixin::findNearestValidAttackTarget))).addAll(VillagerGoalPackages.getIdlePackage(villagerProfession, speedModifier)).build();
+          
+    }
+	
     private static void updateActivity(Villager vill) {
         Brain<Villager> brain = vill.getBrain();
         Activity activity = brain.getActiveNonCoreActivity().orElse(null);
